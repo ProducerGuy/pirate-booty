d9787e75bf5795a9b80ac9cdbc44de99f038230e
Producer Guy <254332139+ProducerGuy@users.noreply.github.com>
2026-09-07 07:36:44 -0500

    fix(play): show Start menu on load (was hidden -> nothing to tap); build 7, bump SW cache

--- play/sw.js
+++ play/sw.js
@@ -1,5 +1,7 @@
-const CACHE = "broadside-v6";
+const CACHE = "broadside-v7";
 const ASSETS = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png"];
 self.addEventListener("install", (e) => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); });
-self.addEventListener("activate", (e) => e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))));
+self.addEventListener("activate", (e) => e.waitUntil(
+  caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim())
+));
 self.addEventListener("fetch", (e) => e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request))));
